f6d6ba9c75495b2bffd4dc733c5237f27f2a3c2e
Rafael Rodrigues Segalla <rrsegalla@gmail.com>
2022-08-16 23:49:28 -0300

    Query method - Search post by title

--- src/main/java/com/rafaelsegalla/workshopspringbootmongodb/repository/PostRepository.java
+++ src/main/java/com/rafaelsegalla/workshopspringbootmongodb/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.rafaelsegalla.workshopspringbootmongodb.repository;
 
 import com.rafaelsegalla.workshopspringbootmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
