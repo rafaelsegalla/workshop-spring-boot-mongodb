014d4984dac83a62a9f00cc4e95be9e38184b230
Rafael Rodrigues Segalla <rrsegalla@gmail.com>
2022-08-16 22:12:42 -0300

    UserDTO

--- src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.rafaelsegalla.workshopspringbootmongodb.resources;
 
 import com.rafaelsegalla.workshopspringbootmongodb.domain.User;
+import com.rafaelsegalla.workshopspringbootmongodb.dto.UserDTO;
 import com.rafaelsegalla.workshopspringbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,7 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(this.userService.findAll());
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> userList = this.userService.findAll();
+        List<UserDTO> userDTOList = userList.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
     }
 }
