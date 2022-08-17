ec5af7495ce1e6db58fa8296a68f2968461e4170
Rafael Rodrigues Segalla <rrsegalla@gmail.com>
2022-08-16 21:39:24 -0300

    User entity and User resource

---
+++ src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.rafaelsegalla.workshopspringbootmongodb.resources;
+
+import com.rafaelsegalla.workshopspringbootmongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria", "maria@gmail.com");
+        User alex = new User("1", "Alex", "alex@gmail.com");
+
+        return ResponseEntity.ok().body(Arrays.asList(maria, alex));
+    }
+}
