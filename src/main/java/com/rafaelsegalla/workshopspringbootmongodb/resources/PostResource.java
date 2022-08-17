698570d09071a64ef5bf27d0d3cf05ac954c351a
Rafael Rodrigues Segalla <rrsegalla@gmail.com>
2022-08-16 23:25:12 -0300

    PostResource and PostService

--- src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/PostResource.java
@@ -1,16 +1,13 @@
 package com.rafaelsegalla.workshopspringbootmongodb.resources;
 
 import com.rafaelsegalla.workshopspringbootmongodb.domain.Post;
-import com.rafaelsegalla.workshopspringbootmongodb.domain.User;
-import com.rafaelsegalla.workshopspringbootmongodb.dto.UserDTO;
 import com.rafaelsegalla.workshopspringbootmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/posts")
