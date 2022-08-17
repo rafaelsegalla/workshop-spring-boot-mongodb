51624712f2eda933c80014555f80158f1fe1f396
Rafael Rodrigues Segalla <rrsegalla@gmail.com>
2022-08-16 23:45:11 -0300

    Query method - Search post by title

--- src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/PostResource.java
+++ src/main/java/com/rafaelsegalla/workshopspringbootmongodb/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.rafaelsegalla.workshopspringbootmongodb.resources;
 
 import com.rafaelsegalla.workshopspringbootmongodb.domain.Post;
+import com.rafaelsegalla.workshopspringbootmongodb.resources.util.URL;
 import com.rafaelsegalla.workshopspringbootmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -16,10 +16,16 @@ public class PostResource {
     @Autowired
     private PostService postService;
 
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = this.postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/title-search")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParams(text);
+        List<Post> postList = postService.findByTitle(text);
+        return ResponseEntity.ok().body(postList);
     }
 }
--- src/main/java/com/rafaelsegalla/workshopspringbootmongodb/services/PostService.java
+++ src/main/java/com/rafaelsegalla/workshopspringbootmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.rafaelsegalla.workshopspringbootmongodb.services.exception.ObjectNotF
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,5 +23,9 @@ public class PostService {
         }
 
         return post.get();
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
